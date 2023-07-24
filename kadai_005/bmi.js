f9e1e35b5b2d38793617abefe6ab4c86ffae2db4
takashi-saito1226 <139130594+takashi-saito1226@users.noreply.github.com>
2023-07-24 21:09:39 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,2 +1,17 @@
 
+let userName;
+
+userName = '体重';
+
+let userNumber = 68;
+
+console.log(userName);
+console.log(userNumber);
+
+userName = '身長';
+userNumber = 1.7;
+
+console.log(userName);
+console.log(userNumber);
+
 console.log(68 / (1.7 * 1.7))

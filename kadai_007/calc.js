a1662034326b9f5fa5f61f82250b2505b15e428f
takashi-saito1226 <139130594+takashi-saito1226@users.noreply.github.com>
2023-07-24 21:15:26 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,16 @@
+let num = 3;
+
+switch (true) {
+case num % 3 == 0 && num % 5 == 0:
+console.log("３と５の倍数です");
+break;
+case num % 5 == 0:
+console.log("５の倍数です");
+break;
+case num % 3 == 0:
+console.log("３の倍数です");
+break;
+default:
+console.log(num);
+break;
+}
